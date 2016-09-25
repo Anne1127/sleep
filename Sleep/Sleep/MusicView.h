d5405534f46c184e4916a0771133ec1b08575290
anne <374804759@qq.com>
2016-09-25 14:31:35 +0800

    sass

---
+++ Sleep/Sleep/MusicView.h
@@ -0,0 +1,13 @@
+//
+//  MusicView.h
+//  Sleep
+//
+//  Created by anne on 16/9/25.
+//  Copyright © 2016年 Anne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicView : UIView
+
+@end
---
+++ Sleep/Sleep/MusicView.m
@@ -0,0 +1,21 @@
+//
+//  MusicView.m
+//  Sleep
+//
+//  Created by anne on 16/9/25.
+//  Copyright © 2016年 Anne. All rights reserved.
+//
+
+#import "MusicView.h"
+
+@implementation MusicView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
